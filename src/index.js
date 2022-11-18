bbec5d37f6562021bd8c413921df9366028e6ca7
SiRi0000 <siriporn.senma@gmail.com>
2022-11-18 10:29:42 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,72 @@
+//Detecting Button Press
+
+for(var i = 0; i<document.querySelectorAll(".drum").length; i++){
+// adding eventlistener to specific element
+    document.querySelectorAll(".drum")[i].addEventListener("click",   function () {
+        var button1 = this.innerHTML;
+
+        makeSoung(button1);
+        buttonAnimation(button1);
+
+    });
+}
+
+
+//Detecting Keypress
+//Adding to the whole doc
+
+
+document.addEventListener("keydown", function (event){
+    makeSoung(event.key);
+    buttonAnimation(event.key);
+});
+
+
+
+function makeSoung(key){
+
+    switch (key){
+        case "w":
+            var audio = new Audio("sounds/crash.mp3")
+            audio.play();
+            break;
+        case "a":
+            var audio1 = new Audio("sounds/kick-bass.mp3")
+            audio1.play();
+            break;
+
+        case "s":
+            var audio2 = new Audio("sounds/snare.mp3")
+            audio2.play();
+            break;
+        case "d":
+            var audio3 = new Audio("sounds/tom-1.mp3")
+            audio3.play();
+            break;
+
+        case "j":
+            var audio4 = new Audio("sounds/tom-2.mp3")
+            audio4.play();
+            break;
+        case "k":
+            var audio5 = new Audio("sounds/tom-3.mp3")
+            audio5.play();
+            break;
+        case "l":
+            var audio6 = new Audio("sounds/tom-4.mp3")
+            audio6.play();
+            break;
+    }
+
+}
+
+function buttonAnimation(currentKey){
+
+
+    var activeButton = document.querySelector("."+currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){activeButton.classList.remove("pressed")
+    }, 100);
+}
